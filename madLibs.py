f87c6065556249e630276c7840260d3c5be09ab0
= <zainr7989@gmail.com>
2019-08-29 17:08:38 -0700

    Add a better function to initialize story blanks

--- madLibs.py
+++ madLibs.py
@@ -1,3 +1,4 @@
+'''
 def replace_blanks(blank_words, user_responses):
     for i in range(len(blank_words)):
         blank_words[i] += user_responses[i]
@@ -7,21 +8,35 @@ def print_full_story(story, responses):
     for i in range(len(story)):
         print(story[i] + responses[i])
 
+replace_blanks(story_list, blanks)
+print_full_story(story_list, blanks)
+'''
+
+
+def init_blanks(dict):
+    list_of_keys = list(dict.keys())
+    for key in list_of_keys:
+        dict[key] = input(key)
+
+
+def print_full_story(story):
+    for line in story:
+        print(line)
+
 
 blanks = {
-    "(insert noun)": "bottle",
-    "(insert adjective)": "smelly",
-    "(insert verb)": "run"
+    "enter a noun: ": "",
+    "enter a verb: ": "",
+    "enter an adjective: ": "",
+    "enter an adverb: ": "",
+    "enter a preposition: ": ""
 }
 
 story_list = [
-    "This is the first part of my story. Please",
+    "This is the first of my story",
     "This is the second part of my story. Please",
     "This is the third part of my story. Please"
 ]
 
-for line in story_list:
-    print(line)
-
-replace_blanks(story_list, blanks)
-print_full_story(story_list, blanks)
+init_blanks(blanks)
+print_full_story(story_list)
